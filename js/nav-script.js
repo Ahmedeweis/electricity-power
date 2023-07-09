211b54d90299868bc7290272a3f73aed8edd9fb4
Ahmed eweis <Ahmedeweiso@gmail.com>
2023-07-09 15:54:58 +0300

    up bills

--- js/nav-script.js
+++ js/nav-script.js
@@ -1,5 +1,26 @@
+/* 0 nav bar script */
 const toggleButton = document.getElementById("toggle-button");
 const naviList = document.getElementById("navi-list");
 toggleButton.addEventListener("click", () => {
   naviList.classList.toggle("active");
 });
+
+
+/* date formate script */
+const startDateInput = document.getElementById('start-date');
+const endDateInput = document.getElementById('end-date');
+
+// Convert the date to the desired format (dd-mm-yyyy)
+const formatDate = (date) => {
+  const d = new Date(date);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const year = d.getFullYear().toString();
+  return `${day}-${month}-${year}`;
+};
+
+// Set the formatted date as the input value
+startDateInput.value = formatDate(startDateInput.value);
+endDateInput.value = formatDate(endDateInput.value);
+
+/* */
